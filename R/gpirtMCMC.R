cdfe06205254cadfa7fae770a62059a0264dff58
JB Duck-Mayr <j.duckmayr@gmail.com>
2019-05-19 08:33:43 -0500

    Improve R interface to the C++ sampling function
    I created a new R interface to the C++ sampling function that gives the
    user more flexibility and control. They no longer have to pre-process
    the responses, but do have to provide the values corresponding to yea
    votes and nay votes if they are other than 0 and 1 or -1 and 1
    respectively. The user can now also specify the scale factor and length
    scale for the GP's covariance function.
    
    I also had to fix a small typo on the Authors@R field in DESCRIPTION

---
+++ R/gpirtMCMC.R
@@ -0,0 +1,35 @@
+#' Gaussian Process IRT MCMC
+#'
+#' Provides posterior samples for the GP-IRT.
+#'
+#' @param y A matrix of responses
+#' @param sample_iterations An integer vector of length one giving the number
+#'   of samples to record
+#' @param burn_iterations An integer vector of length one giving the number of
+#'   burn in (unrecorded) iterations
+#' @param yea_code A vector of length one giving the value corresponding to
+#'   a "yea" response; default is 1
+#' @param nay_code A vector of length one giving the value corresponding to
+#'   a "nay" response; default is 0
+#' @param sf A numeric vector of length one giving the scale factor for the
+#'   covariance function for the Gaussian process prior; default is 1
+#' @param ell A numeric vector of length one giving the length scale for the
+#'   covariance function for the Gaussian process prior; default is 1
+#'
+#' @return A list of length two; the first element, "theta", is a matrix of
+#'   dimensions sample_iterations x n giving the theta parameter draws, and the
+#'   second element, "f", is an array of dimensions n x m x sample_iterations
+#'   giving the f(theta) parameter draws.
+#' @export
+gpirtMCMC <- function(y, sample_iterations, burn_iterations, yea_code = 1,
+                      nay_code = 0, sf = 1, ell = 1) {
+    # First we fix the responses so that yeas are 1 and nays are -1.
+    # We copy the responses in case the nay_code is 1 or yea_code is -1
+    tmp <- responses
+    responses[which(tmp == yea_code)] <- 1
+    responses[which(tmp == nay_code)] <- -1
+    # Now we can call the C++ sampler function
+    result <- .gpirtMCMC(y, sample_iterations, burn_iterations, sf, ell)
+    # And return the result
+    return(result)
+}
--- src/gpirtMCMC.cpp
+++ src/gpirtMCMC.cpp
@@ -1,30 +1,16 @@
 #include "gpirt.h"
 
-//' Gaussian Process IRT MCMC
-//'
-//' Provides posterior samples for the GP-IRT.
-//'
-//' @param y A matrix of responses; each entry should be -1 or 1
-//' @param sample_iterations An integer vector of length one giving the number
-//'   of samples to record
-//' @param burn_iterations An integer vector of length one giving the number of
-//'   burn in (unrecorded) iterations
-//'
-//' @return A list of length two; the first element, "theta", is a matrix of
-//'   dimensions sample_iterations x n giving the theta parameter draws, and the
-//'   second element, "f", is an array of dimensions n x m x sample_iterations
-//'   giving the f(theta) parameter draws.
-//' @export
-// [[Rcpp::export]]
+// [[Rcpp::export(.gpirtMCMC)]]
 Rcpp::List gpirtMCMC(const arma::mat& y, const int sample_iterations,
-                     const int burn_iterations) {
+                     const int burn_iterations, const double sf,
+                     const double ell) {
     int n = y.n_rows;
     int m = y.n_cols;
     int total_iterations = sample_iterations + burn_iterations;
     // Draw initial values of theta and f
     arma::vec mean_zeros = arma::zeros<arma::vec>(n);
     arma::vec theta = rmvnorm(1, mean_zeros, arma::eye<arma::mat>(n, n)).t();
-    arma::mat S = K(theta, theta, 1.0, 1.0);
+    arma::mat S = K(theta, theta, sf, ell);
     S.diag() += 0.001;
     arma::mat f = rmvnorm(m, mean_zeros, S).t();
     // Setup grid
@@ -39,7 +25,7 @@ Rcpp::List gpirtMCMC(const arma::mat& y, const int sample_iterations,
         thetai_prior[i] = R::dnorm(theta_star[i], 0, 1, 1);
     }
     // K(theta_star, theta_star) also doesn't change between iterations
-    arma::mat S11 = K(theta_star, theta_star, 1.0, 1.0);
+    arma::mat S11 = K(theta_star, theta_star, sf, ell);
     // Setup results storage
     arma::mat theta_draws(sample_iterations, n);
     arma::cube f_draws(n, m, sample_iterations);
@@ -47,9 +33,9 @@ Rcpp::List gpirtMCMC(const arma::mat& y, const int sample_iterations,
     for ( int iter = 0; iter < total_iterations; ++iter ) {
         // Draw new parameter values
         f = draw_f(f, y, S);
-        f_star = draw_fstar(f, theta, theta_star, S, S11);
+        f_star = draw_fstar(f, theta, theta_star, S, S11, sf, ell);
         theta = draw_theta(n, theta_star, y, theta0_prior, thetai_prior, f_star);
-        S = K(theta, theta, 1.0, 1.0);
+        S = K(theta, theta, sf, ell);
         S.diag() += 0.001;
         // Store draws
         if ( iter >= burn_iterations ) {
