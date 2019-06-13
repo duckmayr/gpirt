b189da9d1f42dedbe7927a8c438f2df1a062fdaf
JB Duck-Mayr <j.duckmayr@gmail.com>
2019-06-13 12:49:29 -0500

    Attempt algorithm 2.1 from R&W

--- src/draw-theta.cpp
+++ src/draw-theta.cpp
@@ -14,9 +14,17 @@ arma::vec draw_theta(const arma::imat& y,
     arma::vec result(n);
     arma::ivec responses(m);
     arma::vec P(N);
-    arma::mat S01(n-1, 1);
-    arma::mat S10_S00i(1, n-1);
-    arma::vec th_star(1);
+    arma::vec th_star(1);    // a test point (\theta_k^*)
+    arma::mat S01(n-1, 1);   //      K(\theta_{-i}, \theta_k^*)
+    arma::mat S10(1, n-1);   //      K(\theta_k^*, \theta_{-i})
+    arma::mat S00(n-1, n-1); //      K(\theta_{-i}, \theta_{-i})
+    arma::mat L(n-1, n-1);   // chol(K(\theta_{-i}, \theta_{-i}))
+    arma::mat Lt(n-1, n-1);  // chol(K(\theta_{-i}, \theta_{-i}))'
+    arma::mat Alpha(n-1, m); // stores the alpha calculations
+    arma::vec fj(n-1);       // f(-i, j)
+    arma::mat tmp(n-1, 1);   // L  \ fj
+    arma::mat a(n-1, 1);     // L' \ tmp
+    arma::vec v(n-1);        // L  \ S01
     for ( arma::uword i = 0; i < n; ++i ) {
         // For each respondent,
         responses = y.row(i).t();
@@ -24,16 +32,24 @@ arma::vec draw_theta(const arma::imat& y,
         theta_not_i.shed_row(i);
         arma::mat f_not_i = f;
         f_not_i.shed_row(i);
-        arma::mat S00i = K(theta_not_i, theta_not_i, sf_sq, ell_sq_reciprocal);
-        S00i.diag() += 0.000001;
-        S00i = inv_sympd(S00i);
+        S00 = K(theta_not_i, theta_not_i, sf_sq, ell_sq_reciprocal);
+        S00.diag() += 0.000001;
+        L  = arma::chol(S00, "lower");
+        Lt = L.t();
+        for ( arma::uword j = 0; j < m; ++j ) {
+            fj = f_not_i.col(j);
+            tmp = arma::solve(arma::trimatl(L), fj);
+            a = arma::solve(arma::trimatu(Lt), tmp);
+            Alpha.col(j) = a;
+        }
         for ( arma::uword k = 0; k < N; ++k ) {
             // For each value in theta_star,
             // get the log prior + the log likelihood
             th_star[0] = theta_star[k];
             S01        = K(theta_not_i, th_star, sf_sq, ell_sq_reciprocal);
-            S10_S00i   = S01.t() * S00i;
-            double S   = sf_sq - arma::as_scalar(S10_S00i * S01);
+            S10        = S01.t();
+            v          = arma::solve(L, S01);
+            double S   = sf_sq - arma::dot(v, v);
             if ( party[i] ) {
                 P[k] = R_prior[k];
             }
@@ -44,8 +60,7 @@ arma::vec draw_theta(const arma::imat& y,
                 if ( y(i, j) == INT_MIN ) {
                     continue;
                 }
-                arma::vec fj = f_not_i.col(j);
-                double mu_j = arma::as_scalar(S10_S00i * fj);
+                double mu_j = arma::as_scalar(S10 * Alpha.col(j));
                 double mean = mu_j / (std::sqrt(1 + S));
                 P[k] += R::pnorm(mean, 0.0, 1.0, y(i, j), 1);
             }
