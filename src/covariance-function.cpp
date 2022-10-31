aeae5d611c6832aef10adbb08e7f40b7a0c4e023
Yehu Chen <chenyehu@wustl.edu>
2022-10-31 11:20:23 -0500

    supremecourt holdout

--- src/covariance-function.cpp
+++ src/covariance-function.cpp
@@ -28,12 +28,12 @@ arma::mat K_time(const arma::vec& x1, const arma::vec& x2,
     for ( arma::uword j = 0; j < m; ++j ) {
         for ( arma::uword i = 0; i < n; ++i ) {
             double diff = std::abs(x1(i) - x2(j));
-            if (KERNEL.compare("Matern")){
+            if (KERNEL.compare("Matern")==0){
                 result(i, j) = os * os * (1 + std::sqrt(5) * diff / ls + 5 *  diff * diff / ls / ls / 3);
                 result(i, j) = result(i, j) * std::exp(- std::sqrt(5) * diff / ls);
                 result(i, j) += x1(i) * std::pow(theta_prior_sds(1),2) * x2(j);
                 result(i, j) += std::pow(theta_prior_sds(0),2);
-            }else if (KERNEL.compare("RBF")){
+            }else if (KERNEL.compare("RBF")==0){
                 result(i, j) = os * os * std::exp(- diff * diff / ls / ls);
                 result(i, j) += x1(i) * std::pow(theta_prior_sds(1),2) * x2(j);
                 result(i, j) += std::pow(theta_prior_sds(0),2);
